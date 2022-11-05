d168b859a0f4e31f6089695d970d69a1d7329df3
Isah_John <isahjohna@gmail.com>
2022-11-05 02:01:31 -0700

    insertion sort updated

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,22 +12,22 @@
 */
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-    (*n1)->next = n2->next;
-    if (n2->next != NULL)
-        n2->next->prev = *n1;
-    n2->prev = (*n1)->prev;
-    n2->next = *n1;
-    if ((*n1)->prev != NULL)
-        (*n1)->prev->next = n2;
-    else
-        *h = n2;
-    (*n1)->prev = n2;
-    *n1 = n2->prev;
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
 
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
- * 			using the insertion sort algorithm.
+ * using the insertion sort algorithm.
  * @list: A pointer to the head of a doubly-linked list of integers.
  *
  * Description: Prints the list after each swap.
@@ -35,7 +35,7 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *iter, *insert, *tmp;
-	
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
